56d24af1778e7b471f0c7b14242a65c9c9eebbac
KoT0XleB <lapka.roman@yandex.ru>
2022-07-02 23:26:21 +0500

    football working

---
+++ AutoEvent/Events/BallsEvent.cs
@@ -0,0 +1,116 @@
+﻿using MEC;
+using Mirror;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Addons.Models;
+using Qurre.API.Controllers.Items;
+using Qurre.API.Events;
+using Qurre.API.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static AutoEvent.Functions.MainFunctions;
+using Random = UnityEngine.Random;
+
+namespace AutoEvent
+{
+    internal class BallsEvent : Interfaces.IEvent
+    {
+        public string Name => "Дотронься Мячика";
+        public string Description => "";
+        public string Color => "FFFF00";
+        public static Model Model { get; set; }
+        public static Model Balls { get; set; }
+        public static TimeSpan EventTime { get; set; }
+        public int Votes { get; set; }
+        public void OnStart()
+        {
+            Plugin.IsEventRunning = true;
+            Qurre.Events.Round.TeamRespawn += OnTeamRespawning;
+            Qurre.Events.Server.SendingRA += OnSendRA;
+            OnEventStarted();
+        }
+        public void OnStop()
+        {
+            Plugin.IsEventRunning = false;
+            Qurre.Events.Round.TeamRespawn -= OnTeamRespawning;
+            Qurre.Events.Server.SendingRA -= OnSendRA;
+            Timing.CallDelayed(5f, () => EventEnd());
+        }
+        public void OnEventStarted()
+        {
+            CreatingMapFromJson("Death.json", new Vector3(145.18f, 945.26f, -122.97f), out var model);
+            Model = model;
+            //PlayAudio("FallGuys_FallRoll.f32le", 15, true, "Death");
+            Balls = new Model("balls", new Vector3(0, 0, 0));
+            TeleportAndChangeRolePlayers(Player.List.ToList(), RoleType.ClassD, Model.GameObject.transform.position);
+            // Запуск ивента
+            Timing.RunCoroutine(Cycle(), "ballevent_time");
+        }
+        public IEnumerator<float> Cycle()
+        {
+            // Обнуление таймера
+            EventTime = new TimeSpan(0, 0, 0);
+            while (Round.Started)
+            {
+                Balls.AddPart(new ModelPrimitive(
+                    Balls,
+                    PrimitiveType.Sphere,
+                    UnityEngine.Color.red,
+                    Model.GameObject.transform.position + new Vector3(0, 5, 0),
+                    new Vector3(0, 0, 0),
+                    new Vector3(1, 1, 1),
+                    false
+                ));
+                yield return Timing.WaitForSeconds(1f);
+            }
+            OnStop();
+            yield break;
+        }
+        public void EventEnd()
+        {
+            CleanUpAll();
+            if (Audio.Microphone.IsRecording) StopAudio();
+            Log.Info("Запуск удаления");
+            NetworkServer.UnSpawn(Model.GameObject);
+            Timing.RunCoroutine(DestroyObjects(Model));
+            //Player.List.ToList().ForEach(player => player.Role = RoleType.Tutorial);
+            // Рестарт Лобби
+            // EventManager.Init();
+        }
+        // Ивенты
+        public void OnJoin(JoinEvent ev)
+        {
+            ev.Player.Role = RoleType.Spectator;
+        }
+        public void OnTeamRespawning(TeamRespawnEvent ev)
+        {
+            if (Plugin.IsEventRunning) ev.Allowed = false;
+        }
+        public void OnSendRA(SendingRAEvent ev)
+        {
+            if (Plugin.IsEventRunning)
+            {
+                if (Plugin.DonatorGroups.Contains(ev.Player.GroupName))
+                {
+                    ev.Allowed = false;
+                    ev.Success = false;
+                    ev.ReplyMessage = "Сейчас проводится Ивент!";
+                }
+                else if (ev.Name.ToLower() == "server_event")
+                {
+                    if (ev.Args.Count() == 1)
+                    {
+                        if (ev.Args[0].ToLower() == "round_restart")
+                        {
+                            ev.Allowed = false;
+                            EventManager.Harmony.UnpatchAll();
+                            Server.Restart();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AutoEvent/Events/FootballEvent.cs
@@ -0,0 +1,194 @@
+﻿using AutoEvent.Functions;
+using MEC;
+using Mirror;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Addons.Models;
+using Qurre.API.Controllers.Items;
+using Qurre.API.Events;
+using Qurre.API.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static AutoEvent.Functions.MainFunctions;
+using Random = UnityEngine.Random;
+
+namespace AutoEvent
+{
+    internal class FootballEvent : Interfaces.IEvent
+    {
+        public string Name => "Футбольчик";
+        public string Description => "";
+        public string Color => "FFFF00";
+        public static Model Model { get; set; }
+        public static Model Ball { get; set; }
+        public static TimeSpan EventTime { get; set; }
+        public int Votes { get; set; }
+        public int BluePoints { get; set; } = 0;
+        public int RedPoints { get; set; } = 0;
+        public void OnStart()
+        {
+            Plugin.IsEventRunning = true;
+            Qurre.Events.Round.TeamRespawn += OnTeamRespawning;
+            Qurre.Events.Server.SendingRA += OnSendRA;
+            OnEventStarted();
+        }
+        public void OnStop()
+        {
+            Plugin.IsEventRunning = false;
+            Qurre.Events.Round.TeamRespawn -= OnTeamRespawning;
+            Qurre.Events.Server.SendingRA -= OnSendRA;
+            Timing.CallDelayed(10f, () => EventEnd());
+        }
+        public void OnEventStarted()
+        {
+            CreatingMapFromJson("Football.json", new Vector3(145.18f, 945.26f, -122.97f), out var model);
+            Model = model;
+            PlayAudio("FallGuys_DnB.f32le", 7, true, "Футбол");
+            Ball = new Model("balls", new Vector3(0, 0, 0));
+            Ball.AddPart(new ModelPrimitive(Ball, PrimitiveType.Sphere, UnityEngine.Color.red, Model.GameObject.transform.position + new Vector3(0, 5f, 0), new Vector3(0, 0, 0), new Vector3(1, 1, 1)));
+            foreach (var ball in Ball.Primitives)
+            {
+                ball.GameObject.AddComponent<FootballComponent>();
+            }
+            var count = 0;
+            foreach (Player player in Player.List)
+            {
+                if (count % 2 == 0)
+                {
+                    player.Role = RoleType.NtfCaptain;
+                    player.ClearInventory();
+                }
+                else
+                {
+                    player.Role = RoleType.ClassD;
+                    player.ClearInventory();
+                }
+                Timing.CallDelayed(2f, () =>
+                {
+                    player.Position = Model.GameObject.transform.position + new Vector3(0, 5, 0);
+                    player.GameObject.TryGetComponent<BoxCollider>(out BoxCollider collider);
+                    collider.size = new Vector3(1f, 1f, 1f);
+                });
+                count++;
+            }
+            // Запуск ивента
+            Timing.RunCoroutine(Cycle(), "ballevent_time");
+        }
+        public IEnumerator<float> Cycle()
+        {
+            // Обнуление таймера
+            EventTime = new TimeSpan(0, 0, 0);
+            while (BluePoints < 7 && RedPoints < 7 && Player.List.ToList().Count(player => player.Role != RoleType.Spectator) > 1)
+            {
+                foreach(Player player in Player.List)
+                {
+                    var text = string.Empty;
+                    if (player.Role == RoleType.NtfCaptain)
+                    {
+                        text += "<color=cyan>Вы играете за Синюю Команду</color>\n";
+                    }
+                    else
+                    {
+                        text += "<color=red>Вы играете за Красную Команду</color>\n";
+                    }
+                    if (Vector3.Distance(Ball.Primitives[0].Primitive.Position, player.Position) < 5)
+                    {
+                        Ball.Primitives[0].GameObject.TryGetComponent<Rigidbody>(out Rigidbody rig);
+                        rig.AddForce(player.Transform.forward + new Vector3(0, 0.5f, 0), ForceMode.Impulse);
+                    }
+                    player.Broadcast(text + $"<color=blue>{BluePoints}</color> VS <color=red>{RedPoints}</color>", 1);
+                }
+                if (Vector3.Distance(Ball.Primitives[0].Primitive.Position, new Vector3(98.47f, 949.87f, -122.48f)) < 5)
+                {
+                    // Синии ворота
+                    Ball.Primitives[0].Primitive.Position = Model.GameObject.transform.position + new Vector3(0, 5f, 0);
+                    RedPoints++;
+                }
+                if (Vector3.Distance(Ball.Primitives[0].Primitive.Position, new Vector3(191.71f, 949.87f, -123.13f)) < 5)
+                {
+                    // Красные ворота
+                    Ball.Primitives[0].Primitive.Position = Model.GameObject.transform.position + new Vector3(0, 5f, 0);
+                    BluePoints++;
+                }
+                yield return Timing.WaitForSeconds(1f);
+            }
+            if (Player.List.ToList().Count(player => player.Role != RoleType.Spectator) == 1)
+            {
+                BroadcastPlayers($"Недостаточно игроков: Конец Мини-Игры!", 10);
+            }
+            else if (Player.List.ToList().Count(player => player.Role != RoleType.Spectator) == 0)
+            {
+                BroadcastPlayers($"Недостаточно игроков: Конец Мини-Игры!", 10);
+            }
+            else if (BluePoints >= 7)
+            {
+                // 7 очков забито в синие ворота
+                BroadcastPlayers($"<color=red>ПОБЕДА!</color>", 10);
+                foreach(Player player in Player.List)
+                {
+                    if (player.Role == RoleType.NtfCaptain) player.Kill();
+                }
+            }
+            else if (RedPoints >= 7)
+            {
+                // 7 очков забито в красные ворота
+                BroadcastPlayers($"<color=blue>ПОБЕДА!</color>", 10);
+                foreach (Player player in Player.List)
+                {
+                    if (player.Role == RoleType.ClassD) player.Kill();
+                }
+            }
+            OnStop();
+            yield break;
+        }
+        public void EventEnd()
+        {
+            CleanUpAll();
+            if (Audio.Microphone.IsRecording) StopAudio();
+            Log.Info("Запуск удаления");
+            Ball.Destroy();
+            BluePoints = 0;
+            RedPoints = 0;
+            NetworkServer.UnSpawn(Model.GameObject);
+            Timing.RunCoroutine(DestroyObjects(Model));
+            //Player.List.ToList().ForEach(player => player.Role = RoleType.Tutorial);
+            // Рестарт Лобби
+            // EventManager.Init();
+        }
+        // Ивенты
+        public void OnJoin(JoinEvent ev)
+        {
+            ev.Player.Role = RoleType.Spectator;
+        }
+        public void OnTeamRespawning(TeamRespawnEvent ev)
+        {
+            if (Plugin.IsEventRunning) ev.Allowed = false;
+        }
+        public void OnSendRA(SendingRAEvent ev)
+        {
+            if (Plugin.IsEventRunning)
+            {
+                if (Plugin.DonatorGroups.Contains(ev.Player.GroupName))
+                {
+                    ev.Allowed = false;
+                    ev.Success = false;
+                    ev.ReplyMessage = "Сейчас проводится Ивент!";
+                }
+                else if (ev.Name.ToLower() == "server_event")
+                {
+                    if (ev.Args.Count() == 1)
+                    {
+                        if (ev.Args[0].ToLower() == "round_restart")
+                        {
+                            ev.Allowed = false;
+                            EventManager.Harmony.UnpatchAll();
+                            Server.Restart();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
