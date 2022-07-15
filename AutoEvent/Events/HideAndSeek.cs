a85c9df23c1d6a96ef4ae85226d67aa1ef8c2574
KoT0XleB <lapka.roman@yandex.ru>
2022-07-15 23:29:30 +0500

    Update HideAndSeek

--- AutoEvent/Events/HideAndSeek.cs
+++ AutoEvent/Events/HideAndSeek.cs
@@ -18,8 +18,8 @@ namespace AutoEvent.Events
 {
     internal class HideAndSeek : IEvent
     {
-        public string Name => "Догонялки";
-        public string Description => "[В работе!]";
+        public string Name => "Догонялки [В работе!]";
+        public string Description => "Выживание против Догоняющих игроков.";
         public string Color => "FFFF00";
         public string CommandName => "hideandseek";
         public static Model Model { get; set; }
@@ -30,15 +30,19 @@ namespace AutoEvent.Events
         {
             Plugin.IsEventRunning = true;
             Qurre.Events.Player.Join += OnJoin;
-            Qurre.Events.Player.DamageProcess += OnDamageProcess;
+            Qurre.Events.Player.RoleChange += OnRoleChange;
             OnEventStarted();
         }
         public void OnStop()
         {
             Plugin.IsEventRunning = false;
             Qurre.Events.Player.Join -= OnJoin;
-            Qurre.Events.Player.DamageProcess -= OnDamageProcess;
+            Qurre.Events.Player.RoleChange -= OnRoleChange;
             Timing.CallDelayed(5f, () => EventEnd());
+        }
+        public void OnRoleChange(RoleChangeEvent ev)
+        {
+            if (ev.NewRole == RoleType.NtfCaptain || ev.NewRole == RoleType.ClassD) ev.SavePos = true;
         }
         public void OnEventStarted()
         {
@@ -48,14 +52,15 @@ namespace AutoEvent.Events
             TeleportAndChangeRolePlayers(Player.List.ToList(), RoleType.ClassD, Model.GameObject.transform.position + new Vector3(-22.67f, 4.94f, 14.61f));
             // Запуск ивента
             Timing.RunCoroutine(TimeToStart(), "time_to_start");
+            Timing.RunCoroutine(CheckPlayers(), "check_players");
         }
         public IEnumerator<float> TimeToStart()
         {
             foreach (Player player in Player.List)
             {
                 player.GodMode = true;
-                player.EnableEffect("MovementBoost");
-                player.ChangeEffectIntensity("MovementBoost", 4);
+                player.EnableEffect("Scp207");
+                player.ChangeEffectIntensity("Scp207", 4);
             }
 
             for (int time = 10; time != 0; time--)
@@ -68,31 +73,19 @@ namespace AutoEvent.Events
         }
         public IEnumerator<float> Cycle()
         {
-            EventTime = new TimeSpan(0, 60, 0);
+            EventTime = new TimeSpan(0, 0, 30);
 
             InitRandomPlayer();
 
             while (EventTime.TotalSeconds != 0)
-            { // таймер и сделать смерть от гранаты
+            {
                 BroadcastPlayers($"<color=#D71868><b><i>{Name}</i></b></color>\n" +
                 $"<color=yellow>Осталось людей: <color=orange>{Player.List.Count(r => r.Role == RoleType.ClassD)}</color></color>\n" +
                 $"<color=yellow>Осталось секунд: <color=red>{EventTime.Minutes}:{EventTime.Seconds}</color></color>", 1);
 
                 foreach (Player player in Player.List.Where(r => r.Team == Team.MTF))
                 {
-                    foreach (Player anyPlayer in Player.List.Where(r => r.Team == Team.CDP))
-                    {
-                        if (Vector3.Distance(player.GameObject.transform.position, anyPlayer.GameObject.transform.position) < 1)
-                        {
-                            BlockAndChangeRolePlayer(player, RoleType.ClassD);
-                            player.ShowHint("<color=orange>Вы стали Убегающим</color>", 2);
-                            player.ClearInventory();
-
-                            BlockAndChangeRolePlayer(anyPlayer, RoleType.NtfCaptain);
-                            player.ShowHint("<color=red>Вы стали Догоняющим</color>", 2);
-                            anyPlayer.ClearInventory();
-                        }
-                    }
+                    player.ShowHint("<color=yellow>Нажмите <color=red>[V]</color> возле игрока.</color>", 1);
                 }
                 yield return Timing.WaitForSeconds(1f);
                 EventTime -= TimeSpan.FromSeconds(1f);
@@ -106,7 +99,6 @@ namespace AutoEvent.Events
             {
                 // Победитель
                 var player = Player.List.ToList().First(r => r.Role != RoleType.Spectator);
-                player.Hp = 1000;
                 BroadcastPlayers($"<color=#D71868><b><i>{Name}</i></b></color>\n" +
                 $"<color=yellow>ПОБЕДИТЕЛЬ - <color=red>{player.Nickname}</color></color>", 10);
             }
@@ -120,11 +112,34 @@ namespace AutoEvent.Events
             }
             else
             {
-                // Ничья
                 BroadcastPlayers($"<color=#D71868><b><i>{Name}</i></b></color>\n" +
                 $"<color=yellow>Все игроки были убиты(</color>\n", 10);
             }
             OnStop();
+            yield break;
+        }
+        public IEnumerator<float> CheckPlayers()
+        {
+            while (Player.List.Count(r => r.Role != RoleType.Spectator) > 1)
+            {
+                foreach (Player player in Player.List.Where(r => r.Team == Team.CDP))
+                {
+                    foreach (Player anyPlayer in Player.List.Where(r => r.Team == Team.CDP))
+                    {
+                        if (Vector3.Distance(player.GameObject.transform.position, anyPlayer.GameObject.transform.position) < 2)
+                        {
+                            player.BlockSpawnTeleport = true;
+                            player.Role = RoleType.NtfCaptain;
+                            player.ClearInventory();
+
+                            anyPlayer.BlockSpawnTeleport = true;
+                            anyPlayer.Role = RoleType.ClassD;
+                            anyPlayer.ClearInventory();
+                        }
+                    }
+                }
+                yield return Timing.WaitForSeconds(3f);
+            }
             yield break;
         }
         public void RestartEvent()
@@ -155,9 +170,13 @@ namespace AutoEvent.Events
         {
             for (int i = 0; i < count; i++)
             {
-                Player player = Player.List.ToList().RandomItem();
-                BlockAndChangeRolePlayer(player, RoleType.NtfCaptain);
+                List<Player> list = Player.List.Where(r => r.Role != RoleType.Spectator).ToList();
+                Player player = list.RandomItem();
+                //player.Role == RoleType.NtfCaptain;
                 player.ClearInventory();
+
+                player.EnableEffect("Scp207");
+                player.ChangeEffectIntensity("Scp207", 4);
             }
         }
         public void GrenadeDeath(Player player, string Reason)
@@ -182,17 +201,6 @@ namespace AutoEvent.Events
             Timing.RunCoroutine(CleanUpAll());
         }
         // Ивенты
-        public void OnDamageProcess(DamageProcessEvent ev)
-        {
-            if (ev.DamageType == DamageTypes.Falldown)
-            {
-                ev.Amount = 30;
-                if (ev.Target.Hp <= 1)
-                {
-                    GrenadeDeath(ev.Target, "<color=red>Умер от падения</color>");
-                }
-            }
-        }
         public void OnJoin(JoinEvent ev)
         {
             ev.Player.Role = RoleType.Spectator;
