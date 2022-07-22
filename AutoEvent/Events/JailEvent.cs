7fa12f7dd596d18d25c5428510ebaec8ba7f3aa7
KoT0XleB <lapka.roman@yandex.ru>
2022-07-22 22:54:58 +0500

    rework jail

--- AutoEvent/Events/JailEvent.cs
+++ AutoEvent/Events/JailEvent.cs
@@ -27,8 +27,6 @@ namespace AutoEvent.Events
         public string Description => "Режим Jail, в котором нужно проводить мероприятия.";
         public string Color => "FFFF00";
         public string CommandName => "jail";
-
-        public static string DayWeek = string.Empty;
         public static Model Maps { get; set; }
         public static Model Button { get; set; }
         public static Model Doors { get; set; }
@@ -44,8 +42,7 @@ namespace AutoEvent.Events
             Plugin.IsEventRunning = true;
             Qurre.Events.Player.RoleChange += OnChangeRole;
             Qurre.Events.Player.Shooting += OnShootEvent;
-            Qurre.Events.Player.InteractLocker += OnInteractLocker;
-            Qurre.Events.Player.PickupItem += OnPickupItem;
+            Qurre.Events.Player.InteractGenerator += OnInteractGenerator;
             OnEventStarted();
         }
         public void OnStop()
@@ -53,11 +50,9 @@ namespace AutoEvent.Events
             Plugin.IsEventRunning = false;
             Qurre.Events.Player.RoleChange -= OnChangeRole;
             Qurre.Events.Player.Shooting -= OnShootEvent;
-            Qurre.Events.Player.InteractLocker -= OnInteractLocker;
-            Qurre.Events.Player.PickupItem -= OnPickupItem;
-            Timing.CallDelayed(5f, () => EventEnd());
+            Qurre.Events.Player.InteractGenerator -= OnInteractGenerator;
+            Timing.CallDelayed(10f, () => EventEnd());
         }
-
         public void OnEventStarted()
         {
             // Создание карты
@@ -80,37 +75,31 @@ namespace AutoEvent.Events
             {
                 PlayAudio("Instruction.f32le", 40, false, "Instruction");
             });*/
-            // Новый раунд ивента
             WaitingEvent();
         }
         public void WaitingEvent()
         {
-            // День недели новый
-            DayWeek = RandomMessage.RandomItem();
-            // Разделение игроков на тюремщиков и зеков
-            for (int i = 0; i <= Player.List.Count() / 10; i++) // = убрал Правило 1 на 10 человек
+            for (int i = 0; i <= Player.List.Count() / 10; i++)
             {
                 var jailer = Player.List.ToList().RandomItem();
                 jailer.Role = RoleType.NtfCaptain;
                 Timing.CallDelayed(2f, () =>
                 {
-                    jailer.Position = Maps.GameObject.transform.position + RandomPosition(true);
+                    jailer.Position = Maps.GameObject.transform.position + new Vector3(12.194f, 1.56f, -2.59f);
+                    jailer.ClearInventory();
                 });
             }
             foreach (Player player in Player.List)
             {
-                // Спавн
                 if (player.Role != RoleType.NtfCaptain)
                 {
                     player.Role = RoleType.ClassD;
                     Timing.CallDelayed(2f, () =>
                     {
-                        player.Position = Maps.GameObject.transform.position + RandomPosition(false);
+                        player.Position = Maps.GameObject.transform.position + RandomPosition();
                     });
                 }
             }
-            // Очистка предметов у охраны
-            // Запуск ивента
             Timing.RunCoroutine(Cycle(), "jail_time");
         }
         public IEnumerator<float> Cycle()
@@ -124,39 +113,16 @@ namespace AutoEvent.Events
                     $"До начала: <color=red>{time}</color> секунд</color>", 1);
                 yield return Timing.WaitForSeconds(1f);
             }
-            // Пока игроки с обоих команд живы
-            while (Player.List.Count(r => r.Role == RoleType.ClassD) > 0 && Player.List.Count(r => r.Team == Team.MTF) > 0)
+            //Player.List.Count(r => r.Role == RoleType.ClassD) > 0 && Player.List.Count(r => r.Team == Team.MTF) > 0
+            while (!Round.Ended)
             {
-                foreach (var door in JailerDoors.Primitives)
-                {
-                    if (JailerDoorsTime.ContainsKey(door.GameObject))
-                    {
-                        if (JailerDoorsTime[door.GameObject] <= 0)
-                        {
-                            door.GameObject.transform.position -= new Vector3(0f, -5f, 0f);
-                            JailerDoorsTime.Remove(door.GameObject);
-                        }
-                        else JailerDoorsTime[door.GameObject] -= 0.5f;
-                    }
-                    foreach (Player player in Player.List)
-                    {
-                        if (Vector3.Distance(door.GameObject.transform.position, player.Position) < 3)
-                        {
-                            door.GameObject.transform.position += new Vector3(0f, -5f, 0f);
-
-                            if (!JailerDoorsTime.ContainsKey(door.GameObject))
-                            {
-                                JailerDoorsTime.Add(door.GameObject, 2f);
-                            }
-                        }
-                    }
-                }
-                EventTime = new TimeSpan(0, EventTime.Minutes, EventTime.Seconds + 1);
+                PhysicDoors();
                 BroadcastPlayers($"<size=20><color=red>Тюрьма Саймона</color>\n" +
-                    $"<color=yellow>День: <color=cyan>{DayWeek}</color></color>\n" +
-                    $"<color=yellow>Колво Зеков: <color=red>{Player.List.Count(r => r.Role == RoleType.ClassD)}</color></color>\n" +
+                    $"<color=yellow>Зеки: {Player.List.Count(r => r.Role == RoleType.ClassD)}</color> || " +
+                    $"<color=cyan>Охраники: {Player.List.Count(r => r.Team == Team.MTF)}</color>\n" +
                     $"<color=red>{EventTime.Minutes}:{EventTime.Seconds}</color></size>", 1);
-                yield return Timing.WaitForSeconds(1f);
+                yield return Timing.WaitForSeconds(0.5f);
+                EventTime += TimeSpan.FromSeconds(0.5f);
             }
             if (Player.List.Count(r => r.Team == Team.MTF) == 0)
             {
@@ -168,21 +134,16 @@ namespace AutoEvent.Events
                 BroadcastPlayers($"<color=blue><b><i>Победа Охранников</i></b></color>\n" +
                     $"<color=red>{EventTime.Minutes}:{EventTime.Seconds}</color>", 10);
             }
-            // Конец под ивента и рестарт
             OnStop();
             yield break;
         }
-        // Подведение итогов ивента и возврат в лобби
         public void EventEnd()
         {
             if (Audio.Microphone.IsRecording) StopAudio();
-            // Очистка времени
-            EventTime = new TimeSpan(0, 0, 0);
-            DayWeek = string.Empty;
-            JailerDoorsTime.Clear();
             isDoorsOpen = false;
             Server.FriendlyFire = false;
 
+            JailerDoorsTime.Clear();
             Button.Destroy();
             Spawners.Destroy();
             Timing.RunCoroutine(DestroyObjects(Maps));
@@ -190,80 +151,66 @@ namespace AutoEvent.Events
             Timing.RunCoroutine(DestroyObjects(JailerDoors));
             Timing.RunCoroutine(CleanUpAll());
         }
-        public List<string> RandomMessage = new List<string>()
-        {
-            "Понедельник",
-            "Вторник",
-            "Среда",
-            "Четверг",
-            "Пятница",
-            "Суббота"
-        };
-        // Манипуляции с дверьми и примитивами
-        public Vector3 RandomPosition(bool isJailer)
+        public Vector3 RandomPosition()
         {
             Vector3 position = new Vector3(0, 0, 0);
-            if (isJailer)
+            var rand = Random.Range(0, 15);
+            switch (rand)
             {
-                var rand = Random.Range(0, 2);
-                switch (rand)
-                {
-                    case 0: position = new Vector3(12.194f, 1.56f, -2.59f); break;
-                    case 1: position = new Vector3(18.39f, 1.56f, -2.59f); break;
-                }
-            }
-            else
-            {
-                var rand = Random.Range(0, 15);
-                switch (rand)
-                {
-                    case 0: position = new Vector3(6.31f, 2.13f, 44.975f); break;
-                    case 1: position = new Vector3(15.63f, 2.13f, 44.975f); break;
-                    case 2: position = new Vector3(27.14f, 2.13f, 44.975f); break;
-                    case 3: position = new Vector3(37.21f, 2.13f, 44.975f); break;
-                    case 4: position = new Vector3(48.36f, 2.13f, 44.975f); break;
-                    case 5: position = new Vector3(4.04f, 7.13f, 44.975f); break;
-                    case 6: position = new Vector3(14.11f, 7.13f, 44.975f); break;
-                    case 7: position = new Vector3(24.7f, 7.13f, 44.975f); break;
-                    case 8: position = new Vector3(37.4f, 7.13f, 44.975f); break;
-                    case 9: position = new Vector3(48.36f, 7.13f, 44.975f); break;
-                    case 10: position = new Vector3(4.04f, 12.18f, 44.975f); break;
-                    case 11: position = new Vector3(13.38f, 12.18f, 44.975f); break;
-                    case 12: position = new Vector3(25.52f, 12.18f, 44.975f); break;
-                    case 13: position = new Vector3(36.96f, 12.18f, 44.975f); break;
-                    case 14: position = new Vector3(48.04f, 12.18f, 44.975f); break;
-                }
+                case 0: position = new Vector3(6.31f, 2.13f, 44.975f); break;
+                case 1: position = new Vector3(15.63f, 2.13f, 44.975f); break;
+                case 2: position = new Vector3(27.14f, 2.13f, 44.975f); break;
+                case 3: position = new Vector3(37.21f, 2.13f, 44.975f); break;
+                case 4: position = new Vector3(48.36f, 2.13f, 44.975f); break;
+                case 5: position = new Vector3(4.04f, 7.13f, 44.975f); break;
+                case 6: position = new Vector3(14.11f, 7.13f, 44.975f); break;
+                case 7: position = new Vector3(24.7f, 7.13f, 44.975f); break;
+                case 8: position = new Vector3(37.4f, 7.13f, 44.975f); break;
+                case 9: position = new Vector3(48.36f, 7.13f, 44.975f); break;
+                case 10: position = new Vector3(4.04f, 12.18f, 44.975f); break;
+                case 11: position = new Vector3(13.38f, 12.18f, 44.975f); break;
+                case 12: position = new Vector3(25.52f, 12.18f, 44.975f); break;
+                case 13: position = new Vector3(36.96f, 12.18f, 44.975f); break;
+                case 14: position = new Vector3(48.04f, 12.18f, 44.975f); break;
             }
             return position;
         }
-        public List<Vector3> DoorsPosition = new List<Vector3>()
+        // Fuck Doors :D
+        public void PhysicDoors()
         {
-            // 1 этаж
-            new Vector3(8.83f, 2.91f, 41.325f),
-            new Vector3(20.36f, 2.91f, 41.325f),
-            new Vector3(31.42f, 2.91f, 41.325f),
-            new Vector3(42.75f, 2.91f, 41.325f),
-            new Vector3(53.72f, 2.91f, 41.325f),
-            // 2 этаж
-            new Vector3(8.83f, 8.026f, 41.325f),
-            new Vector3(20.36f, 8.026f, 41.325f),
-            new Vector3(31.42f, 8.026f, 41.325f),
-            new Vector3(42.75f, 8.026f, 41.325f),
-            new Vector3(53.72f, 8.026f, 41.325f),
-            // 3 этаж
-            new Vector3(8.83f, 13.08f, 41.325f),
-            new Vector3(20.36f, 13.08f, 41.325f),
-            new Vector3(31.42f, 13.08f, 41.325f),
-            new Vector3(42.75f, 13.08f, 41.325f),
-            new Vector3(53.72f, 13.08f, 41.325f),
-        };
+            foreach (var door in JailerDoors.Primitives)
+            {
+                if (JailerDoorsTime.ContainsKey(door.GameObject))
+                {
+                    if (JailerDoorsTime[door.GameObject] <= 0)
+                    {
+                        door.GameObject.transform.position -= new Vector3(0f, -5f, 0f);
+                        JailerDoorsTime.Remove(door.GameObject);
+                    }
+                    else JailerDoorsTime[door.GameObject] -= 0.5f;
+                }
+                foreach (Player player in Player.List)
+                {
+                    if (Vector3.Distance(door.GameObject.transform.position, player.Position) < 3)
+                    {
+                        door.GameObject.transform.position += new Vector3(0f, -5f, 0f);
+
+                        if (!JailerDoorsTime.ContainsKey(door.GameObject))
+                        {
+                            JailerDoorsTime.Add(door.GameObject, 2f);
+                        }
+                    }
+                }
+            }
+        }
         public void CreatePrisonerDoors()
         {
             Doors = new Model("PrisonerDoors", new Vector3(145.18f, 945.26f, -122.97f));
-            foreach (var pos in DoorsPosition)
-            {
-                Doors.AddPart(new ModelPrimitive(Doors, PrimitiveType.Cube, new Color32(0, 0, 0, 200), pos, Vector3.zero, new Vector3(3.88f, 4.55f, 1)));
-            }
+            Doors.AddPart(new ModelPrimitive(Doors, PrimitiveType.Cube, new Color32(0, 0, 0, 200), new Vector3(8.83f, 8.026f, 41.325f), Vector3.zero, new Vector3(3.88f, 14.75f, 1)));
+            Doors.AddPart(new ModelPrimitive(Doors, PrimitiveType.Cube, new Color32(0, 0, 0, 200), new Vector3(20.36f, 8.026f, 41.325f), Vector3.zero, new Vector3(3.88f, 14.75f, 1)));
+            Doors.AddPart(new ModelPrimitive(Doors, PrimitiveType.Cube, new Color32(0, 0, 0, 200), new Vector3(31.42f, 8.026f, 41.325f), Vector3.zero, new Vector3(3.88f, 14.75f, 1)));
+            Doors.AddPart(new ModelPrimitive(Doors, PrimitiveType.Cube, new Color32(0, 0, 0, 200), new Vector3(42.75f, 8.026f, 41.325f), Vector3.zero, new Vector3(3.88f, 14.75f, 1)));
+            Doors.AddPart(new ModelPrimitive(Doors, PrimitiveType.Cube, new Color32(0, 0, 0, 200), new Vector3(53.72f, 8.026f, 41.325f), Vector3.zero, new Vector3(3.88f, 14.75f, 1)));
         }
         public void CreateJailerDoors()
         {
@@ -278,19 +225,33 @@ namespace AutoEvent.Events
         public static void CreateSpawner()
         {
             Spawners = new Model("Spawner", new Vector3(145.18f, 945.26f, -122.97f));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.RifleRack, new Vector3(15.82f, 1.2f, 3.53f), new Vector3(0f, 180f, 0f), new Vector3(1f, 1f, 1f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.RifleRack, new Vector3(15.82f, 0f, 3.53f), new Vector3(0f, 180f, 0f), new Vector3(1f, 1f, 1f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.RifleRack, new Vector3(15.82f, 2.4f, 3.53f), new Vector3(0f, 180f, 0f), new Vector3(1f, 1f, 1f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.LargeGun, new Vector3(24.83f, 1.02f, -18.76f), new Vector3(0f, -90f, 0f), new Vector3(-1.5f, 1.5f, 1.5f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.RegularMedkit, new Vector3(6.1f, -1.5f, -16.66f), new Vector3(0f, 90f, 0f), new Vector3(2f, 2f, 2f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.RegularMedkit, new Vector3(6.1f, -1.5f, -19.78f), new Vector3(0f, 90f, 0f), new Vector3(2f, 2f, 2f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.RegularMedkit, new Vector3(6.1f, -1.5f, -13.45f), new Vector3(0f, 90f, 0f), new Vector3(2f, 2f, 2f)));
-            Spawners.AddPart(new ModelLocker(Spawners, LockerPrefabs.Pedestal500, new Vector3(20.29f, 0f, -0.25f), new Vector3(0f, -90f, 0f), new Vector3(1f, 1f, 1f)));
+            Spawners.AddPart(new ModelGenerator(Spawners, new Vector3(15.82f, 1.02f, 3.53f), new Vector3(0f, 180f, 0f), new Vector3(2f, 1f, 5f)));
+            Spawners.AddPart(new ModelGenerator(Spawners, new Vector3(24.83f, 1.02f, -18.76f), new Vector3(0f, -90f, 0f), new Vector3(2f, 1f, 5f)));
+            Spawners.AddPart(new ModelGenerator(Spawners, new Vector3(6.1f, 1.02f, -16.66f), new Vector3(0f, 90f, 0f), new Vector3(2f, 1f, 5f)));
         }
         // Ивенты
-        public void OnJoin(JoinEvent ev)
+        public void OnInteractGenerator(InteractGeneratorEvent ev)
         {
-            ev.Player.Role = RoleType.Spectator;
+            if (ev.Generator.GameObject == Spawners.Generators[0].GameObject)
+            {
+                Log.Info("Оружие");
+                ev.Player.ResetInventory(new List<ItemType>
+                {
+                    ItemType.GunE11SR,
+                    ItemType.GunCOM18
+                });
+                ev.Player.AddAhp(100, 100, 0, 0, 0, true);
+            }
+            else if (ev.Generator.GameObject == Spawners.Generators[1].GameObject)
+            {
+                Log.Info("Броня");
+                ev.Player.AddAhp(100, 100, 0, 0, 0, true);
+            }
+            else if (ev.Generator.GameObject == Spawners.Generators[2].GameObject)
+            {
+                Log.Info("Хил");
+                ev.Player.Hp = ev.Player.MaxHp;
+            }
         }
         public void OnShootEvent(ShootingEvent ev)
         {
@@ -316,7 +277,7 @@ namespace AutoEvent.Events
                 ev.Player.Role = RoleType.ClassD;
                 Timing.CallDelayed(2f, () =>
                 {
-                    ev.Player.Position = Maps.GameObject.transform.position + RandomPosition(false);
+                    ev.Player.Position = Maps.GameObject.transform.position + RandomPosition();
                 });
             }
             else if (ev.NewRole == RoleType.NtfPrivate || ev.NewRole == RoleType.NtfSergeant || ev.NewRole == RoleType.NtfSpecialist || ev.NewRole == RoleType.NtfCaptain)
@@ -324,34 +285,10 @@ namespace AutoEvent.Events
                 ev.Player.Role = RoleType.NtfCaptain;
                 Timing.CallDelayed(2f, () =>
                 {
-                    ev.Player.Position = Maps.GameObject.transform.position + RandomPosition(true);
+                    ev.Player.Position = Maps.GameObject.transform.position + new Vector3(12.194f, 1.56f, -2.59f);
                 });
             }
         }
-        public void OnInteractLocker(InteractLockerEvent ev)
-        {
-            /*
-            switch(ev.Locker.Type)
-            {
-                case LockerType.RifleRack: ev.Player.AddItem(ItemType.GunE11SR); break;
-                case LockerType.Pedestal: ev.Player.AddItem(ItemType.GunCOM18); break;
-                case LockerType.RegularMedkit: ev.Player.Hp = ev.Player.MaxHp; break;
-                case LockerType.LargeGun:
-                    {
-                        ev.Player.AddItem(ItemType.ArmorCombat);
-                        ev.Player.Ahp = ev.Player.MaxAhp;
-                        break;
-                    }
-            }
-            ev.Allowed = false;
-            */
-        }
-        public void OnPickupItem(PickupItemEvent ev)
-        {
-            if (ev.Pickup.Type == ItemType.Coin)
-            {
-                ev.Allowed = false;
-            }
-        }
+        public void OnJoin(JoinEvent ev) => ev.Player.Role = RoleType.Spectator;
     }
 }
